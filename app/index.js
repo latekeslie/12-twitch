7721376e18811a5a3b7337fbc33f73881151f806
Kate Leslie <thisizk8@gmail.com>
2017-02-17 15:58:27 -0600

    closing time at the IronYard

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 import 'whatwg-fetch';
+import games from './data';
 // Import `data.json`
-// Import `addGamesToList` from `twitch/add-games-to-list`
+
+import addGamesToList from './twitch/add-games-to-list';
 
 // Look up a `ul` element with a class called `results`
+const grid = document.querySelector('.grid');
+
 // Add games to the results list based on the data from `data.json`
+addGamesToList(grid, games.top);
--- app/twitch/create-game-element.js
+++ app/twitch/create-game-element.js
@@ -1,3 +1,24 @@
-export default null;
+export default function createGameElement(data) {
+  const liItem = document.createElement('li');
+  liItem.classList.add('game-item');
+  liItem.classList.add('grid__item');
 
-document.createMessageItem('div');
+  liItem.innerHTML = `
+  <img src="" alt="" class="game-item__pic">
+  <div class="frame">
+    <h3 class="game-item__name"></h3>
+    <h4 class="game-item__popularity"></h4>
+  </div>`;
+
+  const title = liItem.querySelector('.game-item__name');
+  title.innerText = data.name;
+
+  const popularity = liItem.querySelector('.game-item__popularity');
+  popularity.innerText = (data.popularity) + ' Viewers';
+
+  const pic = liItem.querySelector('.game-item__pic');
+  pic.src = data.box.large;
+  pic.alt = data.name;
+
+  return liItem;
+}
